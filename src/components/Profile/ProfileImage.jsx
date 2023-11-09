553cffe42e370c68168d72542520b8209d1822ef
Horacio <horacio.schumann@gmail.com>
2023-11-08 22:06:30 -0300

    feat: create ProfileImage component

---
+++ src/components/Profile/ProfileImage.jsx
@@ -0,0 +1,11 @@
+export default function ProfileImage() {
+    return (
+        <img
+            src="https://i.ibb.co/yqmCpDZ/eu1.jpg"
+            alt="profile"
+            width={250}
+            height={250}
+            style={{ borderRadius: "50%" }}
+        />
+    );
+}
