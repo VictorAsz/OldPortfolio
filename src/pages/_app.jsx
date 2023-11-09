fed1c14491d09a2bf70f7a7b041d3e7e9bf8b382
Horacio <horacio.schumann@gmail.com>
2023-11-08 22:57:39 -0300

    fix: fix imports

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,19 +1,17 @@
 import "@/styles/globals.css";
 import Head from "next/head";
-import Header from "../../components/Header/index";
-import Footer from "../../components/footer/index";
+import Header from "../components/Header/index";
+import Footer from "../components/footer/index";
 
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-
-    
       <Head>
         <title>Victor | dev</title>
       </Head>
       <Header></Header>
-      <Component {...pageProps} />
+      <Component {...pageProps} />      
       <Footer></Footer>
     </>
 
