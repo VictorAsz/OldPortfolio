f8269e10cf0920a6a2881faa464309fae774c409
Horacio <horacio.schumann@gmail.com>
2023-11-09 00:58:10 -0300

    fix: add styled components

--- src/components/Profile/ProfileImage.jsx
+++ src/components/Profile/ProfileImage.jsx
@@ -1,11 +1,16 @@
+import React from "react";
+import {
+    Img,
+    ImgContainer
+} from "./style";
+
 export default function ProfileImage() {
     return (
-        <img
-            src="https://i.ibb.co/yqmCpDZ/eu1.jpg"
-            alt="profile"
-            width={250}
-            height={250}
-            style={{ borderRadius: "50%" }}
-        />
+        <ImgContainer>
+            <Img
+                src="https://i.ibb.co/yqmCpDZ/eu1.jpg"
+                alt="profile"
+            />
+        </ImgContainer>
     );
 }
